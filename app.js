a929e0c2c6a11da3672c06dde15dc1024d87ac1e
tuuk79 <stevecyoo@gmail.com>
2017-05-07 10:10:38 -0500

    adding print t variable
    fourth step
    
    fifth step
    
    sixth step
    
    seventh step
    
    eighth step
    
    ninth step
    
    tenth step
    
    eleventh step
    
    twelfth step
    
    thirteenth step
    
    fourteenth step

--- app.js
+++ app.js
@@ -5,3 +5,27 @@ console.log('next step after starting app')
 var t = 'test'
 
 console.log(t)
+
+console.log('third step')
+
+console.log('fourth step')
+
+console.log('fifth step')
+
+console.log('sixth step')
+
+console.log('seventh step')
+
+console.log('eighth step')
+
+console.log('ninth step')
+
+console.log('tenth step')
+
+console.log('eleventh step')
+
+console.log('twelfth step')
+
+console.log('thirteenth step')
+
+console.log('fourteenth step')
